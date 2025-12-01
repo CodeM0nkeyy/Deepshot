1c05afc1f86f4032cb0747bb70f839bb77c3a3e0
CodeM0nkeyy <saleeemay@gmail.com>
2025-12-01 13:54:02 +0500

    Add video file with Git LFS and update inference/main.py

--- inference/main.py
+++ inference/main.py
@@ -50,7 +50,7 @@ def main(args):
 
     test_loader = torch.utils.data.DataLoader(dataset_Test,
         batch_size=1, shuffle=False,
-        num_workers=0, pin_memory=True,pin_memory=False,prefetch_factor=None)
+        num_workers=0, pin_memory=False,prefetch_factor=None)
 
     
     # Load the best model and compute its performance
